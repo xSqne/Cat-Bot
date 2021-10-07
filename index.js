153596991b4cb9a8606cef1e8ec5a4120417ca64
xSqne <ileem0519@gmail.com>
2021-10-08 00:57:25 +0200

    Added env var compatiability

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // Require the necessary discord.js classes
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const token;
+
+// Check for token in env vars
+if("TOKEN" in process.env) {
+	console.log('Found token in environmental variables, loading')
+	token = process.env.TOKEN
+} else {
+	token = require('.\config.json')
+}
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -11,7 +19,7 @@ client.commands = new Collection();
 
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
-for (const file of eventFiles) {
+for(const file of eventFiles) {
 	const event = require(`./events/${file}`);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
@@ -22,7 +30,7 @@ for (const file of eventFiles) {
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
+for(const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	// Set a new item in the Collection
 	// With the key as the command name and the value as the exported module
